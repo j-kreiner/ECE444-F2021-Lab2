92c795ee52ea8f241d54d37f2cb8a7ff71ff93c4
Justin Kreiner <j_kreiner@outlook.com>
2021-09-25 17:14:48 -0400

    Activity 4: Added input field for emails

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+from wtforms.fields.html5 import EmailField
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test secret key'
 bootstrap = Bootstrap(app)
@@ -12,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,8 +24,12 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
